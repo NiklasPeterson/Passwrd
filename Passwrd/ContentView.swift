75c1daddc7d99c43a56b734945edfbb1367a99b3
NiklasPeterson <5664205+NiklasPeterson@users.noreply.github.com>
2023-09-19 21:00:39 +0200

    Initial Commit

---
+++ Passwrd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Passwrd
+//
+//  Created by Niklas Peterson on 2023-09-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Passwrd/PasswrdApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswrdApp.swift
+//  Passwrd
+//
+//  Created by Niklas Peterson on 2023-09-19.
+//
+
+import SwiftUI
+
+@main
+struct PasswrdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
